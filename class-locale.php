a4ebdbde4fa435982cb31c79f60cb4ee318dea29
Simon Wheatley <simon@sweetinteraction.com>
2012-01-10 15:44:25 +0000

    Redirect unknown languages to the default language

--- class-locale.php
+++ class-locale.php
@@ -166,9 +166,20 @@ class Babble_Locale {
 			if ( preg_match( $this->lang_regex, $this->get_request_string(), $matches ) )
 				$this->set_lang_from_prefix( $matches[ 0 ] );
 		}
-		// Shouldn't be necessary, but…
+		// So we haven't recognised this language, redirect to the request URI
+		// in the default language.
 		if ( ! $this->lang ) {
-			$this->set_lang( bbl_get_default_lang_code() );
+			if ( ! preg_match( '|^/[^/]+/(.*)?|', $_SERVER[ 'REQUEST_URI' ], $matches ) )
+				return;
+			bbl_switch_to_lang( bbl_get_default_lang_code() );
+			// Annoyingly, the home_url filter may not be set here…
+			add_filter( 'home_url', array( $this, 'home_url' ), null, 2 );
+			$location = trailingslashit( home_url( $matches[ 1 ] ) );
+			bbl_restore_lang();
+			// Non-permanent redirect as we might add this language in the 
+			// future, so don't want agents storing the redirect.
+			wp_redirect( $location, 302 );
+			exit; // You shall not pass (redirection just above, so shouldn't be reached)
 		}
 		// Save for logged in users
 		// @FIXME: Possible additional DB queries here?
