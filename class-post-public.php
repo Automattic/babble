75e455a7836c8c067186fed2ed48587d60162d9a
Simon Wheatley <simon@sweetinteraction.com>
2012-03-13 12:40:45 +0000

    Run prune meta again

--- class-post-public.php
+++ class-post-public.php
@@ -125,7 +125,7 @@ class Babble_Post_Public extends Babble_Plugin {
 		$this->post_types = array();
 		$this->slugs_and_vars = array();
 		
-		$this->version = 5;
+		$this->version = 7;
 
 		// Ensure we catch any existing language shadow post_types already registered
 		$core_post_types = array( 'post', 'page', 'attachment' );
@@ -1789,7 +1789,7 @@ class Babble_Post_Public extends Babble_Plugin {
 		}
 		add_option( "{$option_name}_running", time(), null, 'no' );
 
-		if ( $version < 5 ) {
+		if ( $version < 7 ) {
 			error_log( "Babble Post Public: Start pruning metadata" );
 			$this->prune_post_meta();
 			error_log( "Babble Post Public: Remove excess post meta" );
