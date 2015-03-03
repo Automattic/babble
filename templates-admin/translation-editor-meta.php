6dfef921688a9c7004a955cc54cf142b1d5f1686
Hew <hewsut@gmail.com>
2015-03-03 10:49:27 -0600

    Add more output escaping

--- templates-admin/translation-editor-meta.php
+++ templates-admin/translation-editor-meta.php
@@ -9,10 +9,10 @@ $key         = $original->get_key();
 	<div class="bbl-translation-section">
 
 		<div class="bbl-translation-property bbl-translation-property-meta_key">
-			<?php echo $original->get_input( "bbl_translation[meta][{$key}]", $translation ); ?>
+			<?php echo esc_html( $original->get_input( "bbl_translation[meta][{$key}]", $translation ) ); ?>
 		</div>
 		<div class="bbl-translation-original bbl-translation-original-meta_key">
-			<?php echo $original->get_output(); ?>
+			<?php echo esc_html( $original->get_output() ); ?>
 		</div>
 
 	</div>
