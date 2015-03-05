2c5a5ba6b933185e7133a94cc7180724975f3933
Hew <hewsut@gmail.com>
2015-03-04 20:23:20 -0500

    Remove early escaping
    WP standard is to escape output as late as possible. We follow that for consistency (predictablity).

--- class-meta.php
+++ class-meta.php
@@ -18,7 +18,7 @@ abstract class Babble_Meta_Field {
 	abstract public function get_input( $name, $value );
 
 	public function get_output() {
-		return esc_html( $this->get_value() );
+		return $this->get_value();
 	}
 
 	public function get_title() {
@@ -60,7 +60,7 @@ class Babble_Meta_Field_Textarea extends Babble_Meta_Field {
 	}
 
 	public function get_output() {
-		return nl2br( esc_html( $this->get_value() ) );
+		return $this->get_value();
 	}
 
 }
