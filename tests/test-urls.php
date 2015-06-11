1324f8e5be8c51df148b2c077f6eaebeb18115d6
John Blackbourn <johnbillion@gmail.com>
2015-06-11 13:10:27 +0100

    Update `test_home_url()` so it's now passing.

--- tests/test-urls.php
+++ tests/test-urls.php
@@ -82,15 +82,32 @@ class Test_URLs extends Babble_UnitTestCase {
 	}
 
 	public function test_home_url() {
-		$switched = bbl_switch_to_lang( 'fr' );
+		$switched = bbl_switch_to_lang( 'fr_FR' );
 
 		$this->assertTrue( $switched );
 
-		$this->go_to( home_url( '/fr/' ) ); // trigger a query to populate query_vars
-		$home_url = home_url();
+		// The `query_vars` filter is only triggered once a query has been made, so we need to trigger one.
+		$this->go_to( home_url( '/test/' ) );
+		$home_url = trailingslashit( home_url() );
 
 		$this->assertContains( '/fr/', $home_url );
 
+		$switched = bbl_switch_to_lang( 'en_GB' );
+
+		$this->assertTrue( $switched );
+
+		$home_url = trailingslashit( home_url() );
+
+		$this->assertContains( '/uk/', $home_url );
+
+		// switch back to fr_FR
+		bbl_restore_lang();
+
+		$home_url = trailingslashit( home_url() );
+
+		$this->assertContains( '/fr/', $home_url );
+
+		// switch back to en_US
 		bbl_restore_lang();
 	}
 
