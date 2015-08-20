20cfe4f0a32b4f162477a15a5e90ec16cc1af9e1
Joe Hoyle <joehoyle@gmail.com>
2015-08-20 11:59:08 -0400

    Skip version check for now as it seems ot be failing

--- bin/script.sh
+++ bin/script.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-function version {
-	echo "$@" | gawk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }';
-}
-
-phpv=(`php -v`)
-ver=${phpv[1]}
-echo $(version "$ver")
-
-if [ "$(version "$ver")" -gt "$(version "5.4")" ]; then
-
-	php -S localhost:8000 -t vendor/wordpress -d disable_functions=mail &
-	./bin/behat --profile=travis
-
-fi
+php -S localhost:8000 -t vendor/wordpress -d disable_functions=mail &
+./bin/behat --profile=travis
