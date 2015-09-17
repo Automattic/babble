4fa8d3e2d4dd7d6b8287005d855325909aecafa1
Simon Wheatley <simon@simonwheatley.co.uk>
2015-09-17 20:31:48 +0100

    Babble tests: Search tests: abstract post creation
    Abstract creation of posts and their translations into a
    separate method, so we can reuse the code.

--- tests/test-search.php
+++ tests/test-search.php
@@ -12,30 +12,18 @@ class Test_Babble_Search extends Babble_UnitTestCase {
 		global $wp;
 
 
-		$args = array( 'post_content' => '1 in US English  UBIQUITOUS_WORD UNIQUE_WORD_1_EN_US and some more words which I do no care a jot about and will not search for.' );
-		$en1 = $this->factory->post->create_and_get( $args );
-		$args = array( 'post_content' => '1 in UK English UBIQUITOUS_WORD UNIQUE_WORD_1_EN_GB and some more words which I do no care a jot about and will not search for.' );
-		$uk1 = $this->create_post_translation( $en1, 'en_GB', $args );
-		$args = array( 'post_content' => '1 in French UBIQUITOUS_WORD UNIQUE_WORD_1_FR_FR and some more words which I do no care a jot about and will not search for.' );
-		$fr1 = $this->create_post_translation( $en1, 'fr_FR', $args );
-
-		$args = array( 'post_content' => '2 in US English UBIQUITOUS_WORD UNIQUE_WORD_1_EN_US UNIQUE_WORD_2_EN_US and some more words which I do no care a jot about and will not search for.' );
-		$en2 = $this->factory->post->create_and_get( $args );
-		$args = array( 'post_content' => '2 in UK English UBIQUITOUS_WORD UNIQUE_WORD_1_EN_GB UNIQUE_WORD_2_EN_GB and some more words which I do no care a jot about and will not search for.' );
-		$uk2 = $this->create_post_translation( $en2, 'en_GB', $args );
-		$args = array( 'post_content' => '2 in French UBIQUITOUS_WORD UNIQUE_WORD_1_FR_FR UNIQUE_WORD_2_FR_FR and some more words which I do no care a jot about and will not search for.' );
-		$fr2 = $this->create_post_translation( $en2, 'fr_FR', $args );
+		$posts = $this->create_test_posts();
 
 		// UBIQUITOUS_WORD should be present in both US English posts
 		$this->go_to( '/en/?s=UBIQUITOUS_WORD' );
 		$matching_post_ids = wp_list_pluck( $GLOBALS['wp_query']->posts, 'ID' );
-		$expected_post_ids = wp_list_pluck( array( $en1, $en2 ), 'ID' );
+		$expected_post_ids = wp_list_pluck( array( $posts['en1'], $posts['en2'] ), 'ID' );
 		$this->assertEqualSets( $expected_post_ids, $matching_post_ids );
 
 		// UNIQUE_WORD_2 should only be in the second US English post
 		$this->go_to( '/en/?s=UNIQUE_WORD_2_EN_US' );
 		$matching_post_ids = wp_list_pluck( $GLOBALS['wp_query']->posts, 'ID' );
-		$expected_post_ids = wp_list_pluck( array( $en2 ), 'ID' );
+		$expected_post_ids = wp_list_pluck( array( $posts['en2'] ), 'ID' );
 		$this->assertEqualSets( $expected_post_ids, $matching_post_ids );
 
 		// UNIQUE_WORD_1_FR_FR should be present in no US English posts
@@ -48,13 +36,13 @@ class Test_Babble_Search extends Babble_UnitTestCase {
 		// UBIQUITOUS_WORD should be present in both French posts
 		$this->go_to( '/fr/?s=UBIQUITOUS_WORD' );
 		$matching_post_ids = wp_list_pluck( $GLOBALS['wp_query']->posts, 'ID' );
-		$expected_post_ids = wp_list_pluck( array( $fr1, $fr2 ), 'ID' );
+		$expected_post_ids = wp_list_pluck( array( $posts['fr1'], $posts['fr2'] ), 'ID' );
 		$this->assertEqualSets( $expected_post_ids, $matching_post_ids );
 
 		// UNIQUE_WORD_2_FR_FR should only be in the second French post
 		$this->go_to( '/fr/?s=UNIQUE_WORD_2_FR_FR' );
 		$matching_post_ids = wp_list_pluck( $GLOBALS['wp_query']->posts, 'ID' );
-		$expected_post_ids = wp_list_pluck( array( $fr2 ), 'ID' );
+		$expected_post_ids = wp_list_pluck( array( $posts['fr2'] ), 'ID' );
 		$this->assertEqualSets( $expected_post_ids, $matching_post_ids );
 
 		// UNIQUE_WORD_1 should be present in no French posts
@@ -64,4 +52,25 @@ class Test_Babble_Search extends Babble_UnitTestCase {
 
 	}
 
+
+	protected function create_test_posts() {
+
+		$posts = array();
+
+		$args = array( 'post_content' => '1 in US English  UBIQUITOUS_WORD UNIQUE_WORD_1_EN_US and some more words which I do no care a jot about and will not search for.' );
+		$posts['en1'] = $this->factory->post->create_and_get( $args );
+		$args = array( 'post_content' => '1 in UK English UBIQUITOUS_WORD UNIQUE_WORD_1_EN_GB and some more words which I do no care a jot about and will not search for.' );
+		$posts['uk1'] = $this->create_post_translation( $posts['en1'], 'en_GB', $args );
+		$args = array( 'post_content' => '1 in French UBIQUITOUS_WORD UNIQUE_WORD_1_FR_FR and some more words which I do no care a jot about and will not search for.' );
+		$posts['fr1'] = $this->create_post_translation( $posts['en1'], 'fr_FR', $args );
+
+		$args = array( 'post_content' => '2 in US English UBIQUITOUS_WORD UNIQUE_WORD_1_EN_US UNIQUE_WORD_2_EN_US and some more words which I do no care a jot about and will not search for.' );
+		$posts['en2'] = $this->factory->post->create_and_get( $args );
+		$args = array( 'post_content' => '2 in UK English UBIQUITOUS_WORD UNIQUE_WORD_1_EN_GB UNIQUE_WORD_2_EN_GB and some more words which I do no care a jot about and will not search for.' );
+		$posts['uk2'] = $this->create_post_translation( $posts['en2'], 'en_GB', $args );
+		$args = array( 'post_content' => '2 in French UBIQUITOUS_WORD UNIQUE_WORD_1_FR_FR UNIQUE_WORD_2_FR_FR and some more words which I do no care a jot about and will not search for.' );
+		$posts['fr2'] = $this->create_post_translation( $posts['en2'], 'fr_FR', $args );
+
+		return $posts;
+	}
 }
