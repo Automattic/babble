ce3bffe5f642bde3e8b36b2ab842c00d89a54761
david-binda <david.binovec@gmail.com>
2015-09-28 15:38:58 +0200

    Making the `post_type_descending` method of `SortPosts` class static as it's being used that way in the code

--- translation-group-tool-sorter.php
+++ translation-group-tool-sorter.php
@@ -3,7 +3,7 @@
 // From: http://seancode.blogspot.com/2008/01/php-usort-sort-array-of-objects.html
 
 class SortPosts {
-	function post_type_descending( $m, $n ) {
+	static function post_type_descending( $m, $n ) {
 		if ( $m->post_type == $n->post_type )
 			return 0;
 
